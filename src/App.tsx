ca4816f025c9366640d4cbf0546cc0adabd4da76
bitbytejoy <bitbytejoy@gmail.com>
2022-12-14 15:26:15 +0100

    init: BIG BANG

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from "axios";
+
+type Character = {
+  id: number;
+  name: string;
+  status: string;
+};
 
 function App() {
+  const [dataFetchStatus, setDataFetchStatus] = useState<string>("NOT_STARTED");
+  const [characters, setCharacters] = useState<Character[]>([]);
+  console.log(new Date().getTime(), "App");
+  console.log(new Date().getTime(), "dataFetchStatus", dataFetchStatus);
+  console.log(new Date().getTime(), "characters", characters);
+
+  useEffect(() => {
+    (async () => {
+      console.log(new Date().getTime(), "useEffect");
+      const response = await axios.get("https://rickandmortyapi.com/api/character")
+      console.log(new Date().getTime(), "AXIOS done");
+      setCharacters(response.data.results);
+      setDataFetchStatus("DONE");
+      console.log(new Date().getTime(), "setCharacters(response.data.results)", response.data);
+      console.log(new Date().getTime(), "setDataFetchStatus(DONE)");
+    })();
+    console.log(new Date().getTime(), "setDataFetchStatus(IN_PROGRESS)");
+    setDataFetchStatus("IN_PROGRESS");
+  }, []);
+
+  console.log(new Date().getTime(), "Return JSX");
+
+  let body = <span>No data</span>;
+  switch (dataFetchStatus) {
+    case "IN_PROGRESS": {
+      body = <span>Fetching...</span>
+      break;
+    }
+
+    case "DONE": {
+      body = (
+        <ul>
+          {characters.map(character => (
+            <li
+              key={character.id}
+              style={{marginLeft: "30px"}}
+            >{character.name}</li>
+          ))}
+        </ul>
+      );
+      break;
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"app"}>
+      <div className={"app-data"}>
+        {body}
+      </div>
     </div>
   );
 }
